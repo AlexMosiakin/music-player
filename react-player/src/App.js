d2016f1922fe6b6dc2085e1277f3a8a0fcfa372e
Mamoru <49065044+AlexMosiakin@users.noreply.github.com>
2021-04-27 17:49:54 +0300

    add some basic stuff
    Add components(player, songs), took data, all works fine, need to do the library, next and prev btns, add some animation...

---
+++ react-player/src/App.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+//Adding Components
+import Player from './components/Player'
+import Song from './components/Song'
+//Import styles
+import "./styles/app.scss";
+//Import Util
+import data from './util';
+
+function App() {
+  //State
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+  return (
+    <div className="App">
+      <Song currentSong={currentSong} />
+      <Player 
+      setIsPlaying={setIsPlaying}
+      isPlaying={isPlaying}
+      currentSong={currentSong} 
+      />
+    </div>
+  );
+}
+
+export default App;
